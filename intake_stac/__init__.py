a93b1d40084b58bf53fcadbbbfbd13c7c620e190
Joseph Hamman <jhamman@ucar.edu>
2019-05-09 14:30:15 -0700

    version import order

--- intake_stac/__init__.py
+++ intake_stac/__init__.py
@@ -1,5 +1,5 @@
-from .catalog import StacCatalog, StacCollection, StacItem
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+from .catalog import StacCatalog, StacCollection, StacItem
