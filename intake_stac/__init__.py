8dfe74165c45b5c264c92a39bd7e3a3bfc20616a
Joe Hamman <jhamman@ucar.edu>
2019-10-31 11:04:50 -0700

    add intake.drivers entrypoints for stac objects (#22)
    * add intake.drivers entrypoints for stac objects
    
    * fix entrypoints
    
    * add title to changelog
    
    * more setup.py
    
    * import intake
    
    * make docs pass build
    
    * back to original (but less verbose) doc build
    
    * style

--- intake_stac/__init__.py
+++ intake_stac/__init__.py
@@ -2,4 +2,7 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import intake
 from .catalog import StacCatalog, StacCollection, StacItem
+
+__all__ = ["StacCatalog", "StacCollection", "StacItem"]
