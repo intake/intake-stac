63adffc1d63cf90c8ce3601b7a3749cc1129ee95
Joseph Hamman <jhamman@ucar.edu>
2019-10-31 11:19:07 -0700

    run black on code base

--- intake_stac/catalog.py
+++ intake_stac/catalog.py
@@ -7,7 +7,7 @@ from intake.catalog.local import LocalCatalogEntry
 
 from . import __version__
 
-NULL_TYPE = 'null'
+NULL_TYPE = "null"
 
 
 class AbstractStacCatalog(Catalog):
@@ -32,12 +32,13 @@ class AbstractStacCatalog(Catalog):
             self._stac_obj = self._stac_cls.open(stac_obj)
         else:
             raise ValueError(
-                'Expected %s instance, got: %s' % (type(self._stac_cls),
-                                                   type(stac_obj)))
+                "Expected %s instance, got: %s"
+                % (type(self._stac_cls), type(stac_obj))
+            )
 
-        metadata = self._get_metadata(**kwargs.pop('metadata', {}))
+        metadata = self._get_metadata(**kwargs.pop("metadata", {}))
 
-        name = kwargs.pop('name', self._stac_obj.id)
+        name = kwargs.pop("name", self._stac_obj.id)
 
         super().__init__(name=name, metadata=metadata, **kwargs)
 
@@ -84,7 +85,7 @@ class StacCatalog(AbstractStacCatalog):
         textfiles
     """
 
-    name = 'stac_catalog'
+    name = "stac_catalog"
     _stac_cls = satstac.Catalog
 
     def _load(self):
@@ -100,26 +101,30 @@ class StacCatalog(AbstractStacCatalog):
                     description=collection.title,
                     driver=StacCollection,
                     catalog=self,
-                    args={'stac_obj': collection})
+                    args={"stac_obj": collection},
+                )
         else:
             # in the future this may go away
             for item in self._stac_obj.items():
                 self._entries[item.id] = LocalCatalogEntry(
                     name=item.id,
-                    description='',
+                    description="",
                     driver=StacItem,
                     catalog=self,
-                    args={'stac_obj': item})
+                    args={"stac_obj": item},
+                )
 
     def _get_metadata(self, **kwargs):
-        return dict(description=self._stac_obj.description,
-                    stac_version=self._stac_obj.stac_version,
-                    **kwargs)
+        return dict(
+            description=self._stac_obj.description,
+            stac_version=self._stac_obj.stac_version,
+            **kwargs,
+        )
 
 
 class StacCollection(AbstractStacCatalog):
 
-    name = 'stac_collection'
+    name = "stac_collection"
     _stac_cls = satstac.Collection
 
     def _load(self):
@@ -129,15 +134,22 @@ class StacCollection(AbstractStacCatalog):
         for item in self._stac_obj.items():
             self._entries[item.id] = LocalCatalogEntry(
                 name=item.id,
-                description='',
+                description="",
                 driver=StacItem,
                 catalog=self,
-                args={'stac_obj': item})
+                args={"stac_obj": item},
+            )
 
     def _get_metadata(self, **kwargs):
         metadata = self._stac_obj.properties.copy()
-        for attr in ['title', 'version', 'keywords', 'license',
-                     'providers', 'extent']:
+        for attr in [
+            "title",
+            "version",
+            "keywords",
+            "license",
+            "providers",
+            "extent",
+        ]:
             metadata[attr] = getattr(self._stac_obj, attr, None)
         metadata.update(kwargs)
         return metadata
@@ -145,7 +157,7 @@ class StacCollection(AbstractStacCatalog):
 
 class StacItem(AbstractStacCatalog):
 
-    name = 'stac_item'
+    name = "stac_item"
     _stac_cls = satstac.Item
 
     def _load(self):
@@ -157,7 +169,7 @@ class StacItem(AbstractStacCatalog):
 
     def _get_metadata(self, **kwargs):
         metadata = self._stac_obj.properties.copy()
-        for attr in ['bbox', 'geometry', 'datetime', 'date']:
+        for attr in ["bbox", "geometry", "datetime", "date"]:
             metadata[attr] = getattr(self._stac_obj, attr, None)
         metadata.update(kwargs)
         return metadata
@@ -177,62 +189,73 @@ class StacItem(AbstractStacCatalog):
         and coordinate.
 
         """
-        item = {'concat_dim': 'band', 'urlpath': [], 'type': 'image/x.geotiff'}
+        item = {"concat_dim": "band", "urlpath": [], "type": "image/x.geotiff"}
         titles = []
         assets = self._stac_obj.assets
-        band_info = self._stac_obj.collection().properties.get('eo:bands')
+        band_info = self._stac_obj.collection().properties.get("eo:bands")
 
         for band in bands:
             # band can be band id, name or common_name
             if band in assets:
-                info = next((b for b in band_info if
-                             b.get('id', b.get('name')) == band), None)
+                info = next(
+                    (
+                        b
+                        for b in band_info
+                        if b.get("id", b.get("name")) == band
+                    ),
+                    None,
+                )
             else:
-                info = next((b for b in band_info if
-                             b['common_name'] == band), None)
+                info = next(
+                    (b for b in band_info if b["common_name"] == band), None
+                )
                 if info is not None:
-                    band = info.get('id', info.get('name'))
+                    band = info.get("id", info.get("name"))
 
             if band not in assets or (regrid is False and info is None):
                 valid_band_names = []
                 for b in band_info:
-                    valid_band_names.append(b.get("id", b.get('name')))
+                    valid_band_names.append(b.get("id", b.get("name")))
                     valid_band_names.append(b.get("common_name"))
                 raise ValueError(
-                    f'{band} not found in list of eo:bands in collection.'
-                    f'Valid values: {sorted(list(set(valid_band_names)))}')
+                    f"{band} not found in list of eo:bands in collection."
+                    f"Valid values: {sorted(list(set(valid_band_names)))}"
+                )
 
             value = assets.get(band)
-            band_type = value.get('type')
-            if band_type != item['type']:
+            band_type = value.get("type")
+            if band_type != item["type"]:
                 raise ValueError(
-                    f'Stacking failed: {band} has type {band_type} and '
-                    f'bands must have type {item["type"]}')
+                    f"Stacking failed: {band} has type {band_type} and "
+                    f'bands must have type {item["type"]}'
+                )
 
-            href = value.get('href')
-            pattern = href.replace(band, '{band}')
-            if 'path_as_pattern' not in item:
-                item['path_as_pattern'] = pattern
-            elif item['path_as_pattern'] != pattern:
+            href = value.get("href")
+            pattern = href.replace(band, "{band}")
+            if "path_as_pattern" not in item:
+                item["path_as_pattern"] = pattern
+            elif item["path_as_pattern"] != pattern:
                 raise ValueError(
-                    f'Stacking failed: {href} does not contain '
-                    'band info in a fixed section of the url')
+                    f"Stacking failed: {href} does not contain "
+                    "band info in a fixed section of the url"
+                )
 
             if regrid is False:
-                gsd = info.get('gsd')
-                if 'gsd' not in item:
-                    item['gsd'] = gsd
-                elif item['gsd'] != gsd:
+                gsd = info.get("gsd")
+                if "gsd" not in item:
+                    item["gsd"] = gsd
+                elif item["gsd"] != gsd:
                     raise ValueError(
-                        f'Stacking failed: {band} has different ground '
-                        f'sampling distance ({gsd}) than other bands '
-                        f'({item["gsd"]})')
+                        f"Stacking failed: {band} has different ground "
+                        f"sampling distance ({gsd}) than other bands "
+                        f'({item["gsd"]})'
+                    )
 
-            titles.append(value.get('title'))
-            item['urlpath'].append(href)
+            titles.append(value.get("title"))
+            item["urlpath"].append(href)
 
-        item['title'] = ', '.join(titles)
-        return StacEntry('_'.join(bands), item, stacked=True)
+        item["title"] = ", ".join(titles)
+        return StacEntry("_".join(bands), item, stacked=True)
 
 
 class StacEntry(LocalCatalogEntry):
@@ -245,37 +268,41 @@ class StacEntry(LocalCatalogEntry):
         Construct an Intake catalog entry from a STAC catalog entry.
         """
         driver = self._get_driver(item)
-        super().__init__(name=key,
-                         description=item.get('title', key),
-                         driver=driver,
-                         direct_access=True,
-                         args=self._get_args(item, driver, stacked=stacked),
-                         metadata=item)
+        super().__init__(
+            name=key,
+            description=item.get("title", key),
+            driver=driver,
+            direct_access=True,
+            args=self._get_args(item, driver, stacked=stacked),
+            metadata=item,
+        )
 
     def _get_driver(self, entry):
         drivers = {
-            'application/netcdf': 'netcdf',
-            'image/vnd.stac.geotiff': 'rasterio',
-            'image/vnd.stac.geotiff; cloud-optimized=true': 'rasterio',
-            'image/x.geotiff': 'rasterio',
-            'image/png': "xarray_image",
-            'image/jpg': "xarray_image",
-            'image/jpeg': "xarray_image",
-            'text/xml': 'textfiles',
-            'text/plain': 'textfiles',
-            'text/html': 'textfiles'
+            "application/netcdf": "netcdf",
+            "image/vnd.stac.geotiff": "rasterio",
+            "image/vnd.stac.geotiff; cloud-optimized=true": "rasterio",
+            "image/x.geotiff": "rasterio",
+            "image/png": "xarray_image",
+            "image/jpg": "xarray_image",
+            "image/jpeg": "xarray_image",
+            "text/xml": "textfiles",
+            "text/plain": "textfiles",
+            "text/html": "textfiles",
         }
-        entry_type = entry.get('type', NULL_TYPE)
+        entry_type = entry.get("type", NULL_TYPE)
 
         if entry_type is NULL_TYPE:
-            warnings.warn(f'TODO: handle case with entry without type field. '
-                          ' This entry was: {entry}')
+            warnings.warn(
+                f"TODO: handle case with entry without type field. "
+                " This entry was: {entry}"
+            )
 
         return drivers.get(entry_type, entry_type)
 
     def _get_args(self, entry, driver, stacked=False):
-        args = entry if stacked else {'urlpath': entry.get('href')}
-        if driver in ['netcdf', 'rasterio', 'xarray_image']:
+        args = entry if stacked else {"urlpath": entry.get("href")}
+        if driver in ["netcdf", "rasterio", "xarray_image"]:
             args.update(chunks={})
 
         return args
--- intake_stac/tests/test_catalog.py
+++ intake_stac/tests/test_catalog.py
@@ -10,7 +10,7 @@ from intake_stac.catalog import StacEntry
 
 @pytest.fixture(scope="module")
 def stac_cat_url():
-    return 'https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/catalog.json'  # noqa: F501
+    return "https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/catalog.json"  # noqa: E501
 
 
 @pytest.fixture(scope="module")
@@ -20,12 +20,16 @@ def stac_cat_obj(stac_cat_url):
 
 @pytest.fixture(scope="module")
 def stac_collection_obj():
-    return satstac.Collection.open('https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/eo/sentinel-2-l1c/catalog.json')  # noqa: F501
+    return satstac.Collection.open(
+        "https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/eo/sentinel-2-l1c/catalog.json"  # noqa: E501
+    )
 
 
 @pytest.fixture(scope="module")
 def stac_item_obj():
-    return satstac.Item.open('https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/eo/landsat-8-l1/item.json')  # noqa: F501
+    return satstac.Item.open(
+        "https://raw.githubusercontent.com/sat-utils/sat-stac/master/test/catalog/eo/landsat-8-l1/item.json"  # noqa: E501
+    )
 
 
 @pytest.fixture(scope="module")
@@ -36,29 +40,29 @@ def cat(stac_cat_url):
 def test_init_catalog_from_url(stac_cat_url):
     cat = StacCatalog(stac_cat_url)
     assert isinstance(cat, intake.catalog.Catalog)
-    assert cat.name == 'stac'
-    assert cat.discover()['container'] == 'catalog'
+    assert cat.name == "stac"
+    assert cat.discover()["container"] == "catalog"
 
     cat = StacCatalog.from_url(stac_cat_url)
     assert isinstance(cat, intake.catalog.Catalog)
-    assert cat.name == 'stac'
-    assert cat.discover()['container'] == 'catalog'
+    assert cat.name == "stac"
+    assert cat.discover()["container"] == "catalog"
 
     # test kwargs are passed through
-    cat = StacCatalog.from_url(stac_cat_url, name='intake-stac-test')
-    assert 'intake-stac-test' == cat.name
+    cat = StacCatalog.from_url(stac_cat_url, name="intake-stac-test")
+    assert "intake-stac-test" == cat.name
 
 
 def test_init_catalog_from_satstac_obj(stac_cat_obj):
     cat = StacCatalog(stac_cat_obj)
     assert isinstance(cat, intake.catalog.Catalog)
-    assert cat.discover()['container'] == 'catalog'
-    assert cat.name == 'stac'
+    assert cat.discover()["container"] == "catalog"
+    assert cat.name == "stac"
     assert cat.name == stac_cat_obj.id
 
     # test kwargs are passed through
-    cat = StacCatalog(stac_cat_obj, name='intake-stac-test')
-    assert 'intake-stac-test' == cat.name
+    cat = StacCatalog(stac_cat_obj, name="intake-stac-test")
+    assert "intake-stac-test" == cat.name
 
 
 def test_init_catalog_with_wrong_type_raises(stac_cat_obj):
@@ -68,10 +72,10 @@ def test_init_catalog_with_wrong_type_raises(stac_cat_obj):
 
 def test_init_catalog_with_bad_url_raises():
     with pytest.raises(satstac.STACError):
-        StacCatalog('foo.bar')
+        StacCatalog("foo.bar")
 
 
-@pytest.mark.xfail(reason='TODO: work on nested serialization')
+@pytest.mark.xfail(reason="TODO: work on nested serialization")
 def test_serialize(cat):
     cat_str = cat.serialize()
     assert isinstance(cat_str, str)
@@ -79,66 +83,70 @@ def test_serialize(cat):
 
 def test_cat_entries(cat):
     assert list(cat)
-    assert all([isinstance(v, (LocalCatalogEntry, Catalog))
-                for _, v in cat.items()])
+    assert all(
+        [isinstance(v, (LocalCatalogEntry, Catalog)) for _, v in cat.items()]
+    )
 
 
 def test_cat_name_from_satstac_catalog_id(cat):
-    assert cat.name == 'stac'
+    assert cat.name == "stac"
 
 
 def test_cat_from_collection(stac_collection_obj):
     cat = StacCollection(stac_collection_obj)
-    assert 'L1C_T53MNQ_A017245_20181011T011722' in cat
-    assert 'B05' in cat.L1C_T53MNQ_A017245_20181011T011722
+    assert "L1C_T53MNQ_A017245_20181011T011722" in cat
+    assert "B05" in cat.L1C_T53MNQ_A017245_20181011T011722
 
 
 def test_cat_from_item(stac_item_obj):
     cat = StacItem(stac_item_obj)
     # weird, why is this different than the name above?
-    assert 'B5' in cat
+    assert "B5" in cat
 
 
 def test_cat_item_stacking(stac_item_obj):
     items = StacItem(stac_item_obj)
-    list_of_bands = ['B1', 'B2']
+    list_of_bands = ["B1", "B2"]
     new_entry = items.stack_bands(list_of_bands)
-    assert new_entry.description == 'Band 1 (coastal), Band 2 (blue)'
-    assert new_entry.name == 'B1_B2'
+    assert new_entry.description == "Band 1 (coastal), Band 2 (blue)"
+    assert new_entry.name == "B1_B2"
     new_da = new_entry.to_dask()
-    assert sorted([dim for dim in new_da.dims]) == ['band', 'x', 'y']
+    assert sorted([dim for dim in new_da.dims]) == ["band", "x", "y"]
     assert (new_da.band == list_of_bands).all()
 
 
 def test_cat_item_stacking_using_common_name(stac_item_obj):
     items = StacItem(stac_item_obj)
-    list_of_bands = ['coastal', 'blue']
+    list_of_bands = ["coastal", "blue"]
     new_entry = items.stack_bands(list_of_bands)
-    assert new_entry.description == 'Band 1 (coastal), Band 2 (blue)'
-    assert new_entry.name == 'coastal_blue'
+    assert new_entry.description == "Band 1 (coastal), Band 2 (blue)"
+    assert new_entry.name == "coastal_blue"
     new_da = new_entry.to_dask()
-    assert sorted([dim for dim in new_da.dims]) == ['band', 'x', 'y']
-    assert (new_da.band == ['B1', 'B2']).all()
+    assert sorted([dim for dim in new_da.dims]) == ["band", "x", "y"]
+    assert (new_da.band == ["B1", "B2"]).all()
 
 
 def test_cat_item_stacking_dims_of_different_type_raises_error(stac_item_obj):
     items = StacItem(stac_item_obj)
-    list_of_bands = ['B1', 'ANG']
-    with pytest.raises(ValueError, match=(
-            'ANG not found in list of eo:bands in collection')):
+    list_of_bands = ["B1", "ANG"]
+    with pytest.raises(
+        ValueError, match=("ANG not found in list of eo:bands in collection")
+    ):
         items.stack_bands(list_of_bands)
 
 
-def test_cat_item_stacking_dims_with_nonexistent_band_raises_error(stac_item_obj):  # noqa: F501
+def test_cat_item_stacking_dims_with_nonexistent_band_raises_error(
+    stac_item_obj
+):  # noqa: E501
     items = StacItem(stac_item_obj)
-    list_of_bands = ['B1', 'foo']
+    list_of_bands = ["B1", "foo"]
     with pytest.raises(ValueError, match="'B8', 'B9', 'blue', 'cirrus'"):
         items.stack_bands(list_of_bands)
 
 
 def test_cat_item_stacking_dims_of_different_size_regrids(stac_item_obj):
     items = StacItem(stac_item_obj)
-    list_of_bands = ['B1', 'B8']
+    list_of_bands = ["B1", "B8"]
     B1_da = items.B1.to_dask()
     assert B1_da.shape == (1, 7801, 7641)
     B8_da = items.B8.to_dask()
@@ -146,38 +154,42 @@ def test_cat_item_stacking_dims_of_different_size_regrids(stac_item_obj):
     new_entry = items.stack_bands(list_of_bands, regrid=True)
     new_da = new_entry.to_dask()
     assert new_da.shape == (2, 15601, 15281)
-    assert sorted([dim for dim in new_da.dims]) == ['band', 'x', 'y']
+    assert sorted([dim for dim in new_da.dims]) == ["band", "x", "y"]
     assert (new_da.band == list_of_bands).all()
 
 
-def test_cat_item_stacking_dims_of_different_size_raises_error_by_default(stac_item_obj):  # noqa: F501
+def test_cat_item_stacking_dims_of_different_size_raises_error_by_default(
+    stac_item_obj
+):  # noqa: E501
     items = StacItem(stac_item_obj)
-    list_of_bands = ['B1', 'B8']
+    list_of_bands = ["B1", "B8"]
     B1_da = items.B1.to_dask()
     assert B1_da.shape == (1, 7801, 7641)
     B8_da = items.B8.to_dask()
     assert B8_da.shape == (1, 15601, 15281)
-    with pytest.raises(ValueError, match='B8 has different ground sampling'):
+    with pytest.raises(ValueError, match="B8 has different ground sampling"):
         items.stack_bands(list_of_bands)
 
 
 def test_stac_entry_constructor():
-    key = 'B1'
-    item = {'href': 'https://landsat-pds.s3.amazonaws.com/c1/L8/120/046/LC08_L1GT_120046_20181012_20181012_01_RT/LC08_L1GT_120046_20181012_20181012_01_RT_B1.TIF',  # noqa: F501
-            'type': 'image/x.geotiff',
-            'eo:bands': [0],
-            'title': 'Band 1 (coastal)'}
+    key = "B1"
+    item = {
+        "href": "https://landsat-pds.s3.amazonaws.com/c1/L8/120/046/LC08_L1GT_120046_20181012_20181012_01_RT/LC08_L1GT_120046_20181012_20181012_01_RT_B1.TIF",  # noqa: E501
+        "type": "image/x.geotiff",
+        "eo:bands": [0],
+        "title": "Band 1 (coastal)",
+    }
 
     entry = StacEntry(key, item)
 
     d = entry.describe()
 
-    assert d['name'] == key
-    assert d['container'] == 'xarray'
-    assert d['plugin'] == ['rasterio']
-    assert d['args']['urlpath'] == item['href']
-    assert d['description'] == item['title']
-    assert d['metadata'] == item
+    assert d["name"] == key
+    assert d["container"] == "xarray"
+    assert d["plugin"] == ["rasterio"]
+    assert d["args"]["urlpath"] == item["href"]
+    assert d["description"] == item["title"]
+    assert d["metadata"] == item
 
 
 # TODO - Add tests for:
