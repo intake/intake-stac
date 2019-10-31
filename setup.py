75db525f93e3c6494577eba9322644521e94be37
Joe Hamman <jhamman@ucar.edu>
2019-10-31 11:37:14 -0700

    Dev build (#24)
    * add intake.drivers entrypoints for stac objects (#22)
    
    * add intake.drivers entrypoints for stac objects
    
    * fix entrypoints
    
    * add title to changelog
    
    * more setup.py
    
    * import intake
    
    * make docs pass build
    
    * back to original (but less verbose) doc build
    
    * style
    
    * add dev build to ci

--- setup.py
+++ setup.py
@@ -14,9 +14,16 @@ with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
 
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-PYTHON_REQUIRES = '>=3.5'
+PYTHON_REQUIRES = '>=3.6'
 SETUP_REQUIRES = ['pytest-runner >= 4.2'] if needs_pytest else []
 TESTS_REQUIRE = ['pytest >= 2.7.1']
+ENTRY_POINTS = {
+    'intake.drivers': [
+        'stac_catalog = intake_stac.catalog:StacCatalog',
+        'stac_collection = intake_stac.catalog:StacCollection',
+        'stac_item = intake_stac.catalog:StacItem',
+    ]
+}
 
 description = ("An intake adapter for building intake catalogs begining "
                "with SpatioTemporal Asset Catalogs (STAC)")
@@ -30,10 +37,13 @@ setup(
     url="https://github.com/pangeo-data/intake-stac",
     py_modules=['intake_stac'],
     packages=find_packages(),
+    package_dir={"intake_stac": "intake_stac"},
+    include_package_data=True,
     python_requires=PYTHON_REQUIRES,
     install_requires=INSTALL_REQUIRES,
     setup_requires=SETUP_REQUIRES,
     tests_require=TESTS_REQUIRE,
+    entry_points=ENTRY_POINTS,
     license="BSD 2-Clause",
     zip_safe=False,
     keywords="intake stac",
