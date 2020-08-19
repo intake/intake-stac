9bf0e8eaaf2aaed42ebc675f66e8a8d792405840
Alex Mandel <alex@developmentseed.org>
2020-08-19 09:33:12 -0700

    Update drivers based on feedback

--- intake_stac/catalog.py
+++ intake_stac/catalog.py
@@ -334,7 +334,7 @@ class StacEntry(LocalCatalogEntry):
             'application/x-netcdf': 'netcdf',
             'application/parquet': 'parquet',
             'application/x-parquet': 'parquet',
-            # 'application/x-hdf5': '',
+            'application/x-hdf5': 'netcdf',
             # 'application/x-hdf': '',
             'image/vnd.stac.geotiff': 'rasterio',
             'image/vnd.stac.geotiff; cloud-optimized=true': 'rasterio',
@@ -347,9 +347,9 @@ class StacEntry(LocalCatalogEntry):
             'text/xml': 'textfiles',
             'text/plain': 'textfiles',
             'text/html': 'textfiles',
-            'application/geo+json': 'geopandas',
-            # 'application/json': '?',
+            'application/json': 'textfiles',
             # 'application/geopackage+sqlite3': 'geopandas',
+            'application/geo+json': 'geopandas',
         }
         entry_type = entry.get('type', NULL_TYPE)
 
